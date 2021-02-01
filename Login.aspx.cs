1f623ddc79f9e6868b027619aa36c6240907f96e
Kamila A <69719614+kartyszko@users.noreply.github.com>
2021-02-01 16:00:01 +0100

    end

---
+++ Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            con.Open();
+            string query = "select count(*) from tabluser where user_name ='" + txtuser.Text + "' and pass='" + txtpass.Text +"' ";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            string output = cmd.ExecuteScalar().ToString();
+
+            if (output == "1")
+            {
+                //create a session
+                Session["user"] = txtuser.Text;
+                Response.Redirect("Welcome.aspx");
+            }
+
+            else
+            {
+                Response.Write("Login Faild");
+            }
+        }
+    }
+}
---
+++ addContent.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class addContent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("Home.aspx");
+            } 
+
+}
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("insert into tableAd"+ "(ID,date,ogloszenie,podpis) values (@ID,@date,@ogloszenie,@podpis)", con);
+            cmd.Parameters.AddWithValue("@ID",txtID.Text);
+            cmd.Parameters.AddWithValue("@date", txtDate.Text);
+            cmd.Parameters.AddWithValue("@ogloszenie", txtAd.Text);
+            cmd.Parameters.AddWithValue("@podpis", txtYourName.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Sukces')</script>");
+
+        }
+    }
+}
